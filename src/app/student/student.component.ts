34b3566da575d789678aa8d687ea317f14be904c
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-09-16 23:53:00 +0700

    Implement common style for area

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+  styleUrls: ['./student.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class StudentComponent {
 
