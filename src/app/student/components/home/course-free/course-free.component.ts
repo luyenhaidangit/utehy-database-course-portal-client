856527164c256160a07b9f9dbea4db46042db6e1
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-09-30 19:56:30 +0700

    Fix style common layout course

--- src/app/student/components/home/course-free/course-free.component.ts
+++ src/app/student/components/home/course-free/course-free.component.ts
@@ -3,9 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-course-free',
   templateUrl: './course-free.component.html',
-  styleUrls: [
-    './course-free.component.css',
-    '../course-pro/course-pro.component.css',
-  ],
+  styleUrls: ['./course-free.component.css'],
 })
 export class CourseFreeComponent {}
