ac7a51d13b7d506d57aaed180b539f8d434cb45d
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-10-10 09:05:39 +0700

    Implement interface auth modal

--- src/app/student/components/auth/auth.component.ts
+++ src/app/student/components/auth/auth.component.ts
@@ -1,18 +1,23 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  TemplateRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AuthComponent {
   modalRef?: BsModalRef;
   @ViewChild('template') template!: TemplateRef<any>;
 
-  constructor(private modalService: BsModalService) {
-  }
- 
+  constructor(private modalService: BsModalService) {}
+
   openModal() {
     this.modalRef = this.modalService.show(this.template);
   }
