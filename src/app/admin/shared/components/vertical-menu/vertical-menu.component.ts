5937eefdf1d3fc6742210fea4d3550f16d15f0b5
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-11-20 17:22:40 +0700

    Implement base layout dashboard page

---
+++ src/app/admin/shared/components/vertical-menu/vertical-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-menu',
+  templateUrl: './vertical-menu.component.html',
+  styleUrls: ['./vertical-menu.component.css']
+})
+export class VerticalMenuComponent {
+
+}
