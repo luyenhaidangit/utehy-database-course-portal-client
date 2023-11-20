0f06d5b513e57a59277b40067170b0cf5d8155c4
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-11-20 15:19:16 +0700

    Implement page forgot password, register

--- src/app/admin/components/auth/auth-routing.module.ts
+++ src/app/admin/components/auth/auth-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    children: [
-      {
-        path: '',
-        component: LoginComponent,
-      }
-    ]
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent
   },
 ];
 
--- src/app/admin/components/auth/auth.module.ts
+++ src/app/admin/components/auth/auth.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ForgotPasswordComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
