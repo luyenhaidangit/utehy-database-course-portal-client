a121077cd710c7b1b6941c75df53a248f973a199
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-11-24 23:31:33 +0700

    Implement paging interface list manage teacher

--- src/app/admin/components/teacher/list-teacher/list-teacher.component.ts
+++ src/app/admin/components/teacher/list-teacher/list-teacher.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TeacherService } from 'src/app/admin/services/apis/teacher.service';
+import { PaginationHelperService } from 'src/app/admin/services/helpers/pagination-helper.service';
+import { defaultPerPage } from 'src/app/admin/configs/paging.config';
 
 @Component({
   selector: 'app-list-teacher',
@@ -9,13 +11,23 @@ import { TeacherService } from 'src/app/admin/services/apis/teacher.service';
 export class ListTeacherComponent implements OnInit {
   public teachers: any;
   public selectedItems: number[] = [];
+  public currentPage: any;
+  public pageSize: any;
+  public totalPages: any;
+  public config: { [key: string]: any, defaultPerPage: any[]  } = { defaultPerPage };
 
   constructor(private teacherService: TeacherService) { }
 
   ngOnInit() {
-    this.teacherService.getTeachers().subscribe((result: any) => {
+    this.getTeachers({});
+  }
+
+  getTeachers(request: any) {
+    this.teacherService.getTeachers(request).subscribe((result: any) => {
       if(result.status){
         this.teachers = result.data.items;
+        this.totalPages = result.data.totalPages;
+        this.currentPage = result.data.pageIndex;
       }
     });
   }
@@ -37,6 +49,19 @@ export class ListTeacherComponent implements OnInit {
         this.selectedItems = this.selectedItems.filter((id) => id !== teacherId);
     } else {
         this.selectedItems.push(teacherId);
+    }
+  }
+
+  setPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+
+      const request = {
+        pageIndex: this.currentPage,
+        pageSize: this.pageSize
+      }
+
+      this.getTeachers(request);
     }
   }
 }
---
+++ src/app/admin/configs/paging.config.ts
@@ -0,0 +1,18 @@
+export const defaultPerPage = [
+    {
+        label: 10,
+        value: 10
+    },
+    {
+        label: 25,
+        value: 25
+    },
+    {
+        label: 50,
+        value: 50
+    },
+    {
+        label: 100,
+        value: 100
+    }
+]
