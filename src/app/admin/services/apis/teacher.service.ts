d669eb94d2eb0c208df99436301854576097aeea
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-11-27 15:45:32 +0700

    Update loading for http admin

--- src/app/admin/services/apis/teacher.service.ts
+++ src/app/admin/services/apis/teacher.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpTeacherLoadingService } from 'src/app/shared/services/https/http-teacher-loading.service';
 import { HttpTeacherNotLoadingService } from 'src/app/shared/services/https/http-teacher-not-loading.service';
 
 @Injectable({
   providedIn: 'root', 
 })
 export class TeacherService {
-  constructor(private http: HttpTeacherNotLoadingService) { }
+  constructor(private http: HttpTeacherLoadingService) { }
 
   getTeachers(request: any = null): Observable<any> {
     return this.http.get('teacher/get', request);
