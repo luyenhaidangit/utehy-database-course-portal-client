930690d2caf679c4b96ac876830be56359e0e628
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-12-06 22:21:14 +0700

    Design form add question

--- src/app/admin/components/question/add-question/add-question.component.ts
+++ src/app/admin/components/question/add-question/add-question.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService as NgxToastrService } from 'ngx-toastr';
+import { TeacherService } from 'src/app/admin/services/apis/teacher.service';
+import { Router } from '@angular/router';
+import questionConstant from 'src/app/admin/constants/question.constant';
 
 @Component({
   selector: 'app-add-question',
@@ -6,5 +10,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-question.component.css']
 })
 export class AddQuestionComponent {
+  //Question
+  questionContant: any = questionConstant;
 
+  question: any = {
+    type: 1,
+  };
+
+  //Data
+  public teacher: any = {
+    teacherId: '',
+    name: '',
+    email: '',
+    phone: '',
+    verificationType: 2,
+    status: true,
+  };
+
+  constructor(private ngxToastr: NgxToastrService,private teacherService: TeacherService,private router:Router) { }
+
+  ngOnInit() {
+    
+  }
+
+  onSubmit(){
+    this.teacherService.createTeachers(this.teacher).subscribe((result: any) => {
+      if(result.status){
+        this.ngxToastr.success(result.message,'',{
+          progressBar: true
+        });
+        this.router.navigate(['/admin/teacher']);
+      }
+    },error => {
+      console.log(error);
+      this.ngxToastr.error(error.error.message,'',{
+        progressBar: true
+      });
+    });
+  }
+
+  validVerificationType(): number {
+    if (this.teacher.verificationType === 2) {
+        if (!this.teacher.email) {
+            return -1;
+        } else {
+            return 1;
+        }
+    } else {
+      if (!this.teacher.phone) {
+        return -2;
+      } else {
+        return 1;
+      }
+    }
+  }
 }
---
+++ src/app/admin/constants/question.constant.ts
@@ -0,0 +1,23 @@
+const questionConstant = {
+    types: [
+        {
+            key: 1,
+            value: "Nhiều lựa chọn" 
+        },
+        {
+            key: 2,
+            value: "Nhiều đáp án" 
+        },
+        {
+            key: 3,
+            value: "Đúng/sai" 
+        },
+        {
+            key: 4,
+            value: "Điền vào chỗ trống" 
+        }
+    ]
+};
+    
+export default questionConstant;
+    
