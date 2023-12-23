069e7f25c47e79ccd68566fe04b884c675577a39
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2023-12-23 22:49:26 +0700

    Implement feature manage course database

--- src/app/admin/components/course/course-routing.module.ts
+++ src/app/admin/components/course/course-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListCourseComponent } from './list-course/list-course.component';
+import { InfoCourseComponent } from './info-course/info-course.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListCourseComponent
+  },
+  {
+    path: 'info',
+    component: InfoCourseComponent
   },
 ];
 
--- src/app/admin/components/course/course.module.ts
+++ src/app/admin/components/course/course.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { CourseRoutingModule } from './course-routing.module';
 import { ListCourseComponent } from './list-course/list-course.component';
-
+import { InfoCourseComponent } from './info-course/info-course.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
     ListCourseComponent,
+    InfoCourseComponent,
   ],
   imports: [
     CommonModule,
-    CourseRoutingModule
+    CourseRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CKEditorModule
   ]
 })
 export class CourseModule { }
