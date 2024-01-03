f64e624dcde91ce6edfa60ce127a38e5d05aabe2
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-01-03 23:52:35 +0700

    Implement feature render student

--- src/app/admin/services/apis/student.service.ts
+++ src/app/admin/services/apis/student.service.ts
@@ -1,35 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpStudentLoadingService } from 'src/app/shared/services/https/http-student-loading.service';
+import { HttpAdminLoadingService } from 'src/app/shared/services/https/http-admin-loading.service';
 import { HttpStudentNotLoadingService } from 'src/app/shared/services/https/http-student-not-loading.service';
 
 @Injectable({
   providedIn: 'root', 
 })
 export class StudentService {
-  constructor(private http: HttpStudentLoadingService) { }
+  constructor(private httpAdminLoading: HttpAdminLoadingService) { }
 
   getStudents(request: any = null): Observable<any> {
-    return this.http.getN('student/get', request);
+    return this.httpAdminLoading.get('student/get', request);
   }
 
   createStudents(request: any): Observable<any> {
-    return this.http.post('student/create', request);
+    return this.httpAdminLoading.post('student/create', request);
   }
 
   getStudentById(request: any): Observable<any> {
-    return this.http.getN(`student/get-by-id?id=${request.id}`,{});
+    return this.httpAdminLoading.get(`student/get-by-id?id=${request.id}`,{});
   }
 
   editStudent(request: any): Observable<any> {
-    return this.http.post('student/edit', request);
+    return this.httpAdminLoading.post('student/edit', request);
   }
 
   deleteStudent(request: any): Observable<any> {
-    return this.http.post('student/delete', request);
+    return this.httpAdminLoading.post('student/delete', request);
   }
 
   deleteMultipleStudent(request: any): Observable<any> {
-    return this.http.post('student/delete-multiple', request);
+    return this.httpAdminLoading.post('student/delete-multiple', request);
   }
 }
