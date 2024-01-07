3f550315681465b7bb0fad6631da3ee256265ab4
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-01-07 12:10:39 +0700

    Implement dropdown item

--- src/app/admin/components/group-module/group-module.module.ts
+++ src/app/admin/components/group-module/group-module.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { GroupModuleRoutingModule } from './group-module-routing.module';
 import { ListGroupModuleComponent } from './list-group-module/list-group-module.component';
@@ -13,7 +14,8 @@ import { ListGroupModuleComponent } from './list-group-module/list-group-module.
   imports: [
     CommonModule,
     GroupModuleRoutingModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ]
 })
 export class GroupModuleModule { }
