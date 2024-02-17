a68e6ac4dbe0ec5717e0cba7db02664b28386503
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-02-18 01:39:17 +0700

    Implement layout view

--- src/app/shared/components/left-side-bar/left-side-bar.component.ts
+++ src/app/shared/components/left-side-bar/left-side-bar.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { MetisMenu } from 'metismenujs';
+import SimpleBar from 'simplebar';
 
 @Component({
   selector: 'app-left-side-bar',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./left-side-bar.component.css']
 })
 export class LeftSideBarComponent {
+  currentUrl: string = "";
 
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.currentUrl = event.url;
+    });
+  }
+
+  ngOnInit() {
+    new MetisMenu("#side-menu");
+    new SimpleBar(document.getElementById('simplebar') as HTMLElement);
+    this.currentUrl = this.router.url;
+  }
+
+  isActive(url: string): boolean {
+    return this.router.url.startsWith(url);
+  }
 }
